dac3e17fadb3bc16dfd59488c7137e5abd48fab1
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-10-23 20:18:38 -0300

    feat: realize AdvantagesBlock

---
+++ components/AdvantagesBlock/AdvantagesBlock.js
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import styles from './AdvantagesBlock.module.css';
+import { advantagesData } from '@/data/advantagesData';
+
+export default function AdvantagesBlock() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.advantages}>
+        {advantagesData.map((item) => (
+          <div key={item.id} className={styles.card}>
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.text}>{item.text}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/images/index/advantages.jpg"
+          alt="Интерьер с подсветкой"
+          width={1480}
+          height={562}
+          className={styles.image}
+          quality={90}
+          loading="lazy"
+        />
+      </div>
+    </section>
+  );
+}
